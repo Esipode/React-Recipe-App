040e3ce09d0427f006ceae2ad39010ad16e08d23
Chad Dyck <chad@chaddyck.com>
2020-03-03 09:57:40 -0700

    Added search functionality.

--- src/Search.jsx
+++ src/Search.jsx
@@ -4,16 +4,46 @@ class Search extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			search: ''
+			search: '',
+			hideHeader: false
+		}
+		this.pagePosition = 0;
+	}
+	startSearch = (e) => {
+		e.preventDefault();
+		if (this.state.search === '' || this.props.isLoading) {
+			return
+		}
+		else {
+			this.props.getRecipeCallback(this.state.search);
+			return this.setState({
+				search: ''
+			})
+		}
+		
+	}
+	searchUpdate = (e) => {
+		if (this.props.isLoading) {
+			return
+		}
+		else {
+			let srcVal = e.target.value;
+			const regExp = /[^\w\s-]/g;
+			if (srcVal.match(regExp)) {
+				srcVal = srcVal.replace(regExp, '');
+			}
+			return this.setState({
+				search: srcVal
+			})
 		}
 	}
 	render() {
 		return (
-			<div>
+			<div className={this.state.hideHeader ? "header hideHeader" : "header"}>
 				<form className="searchContainer">
 					<img src="./logo.svg" alt="logo" draggable="false"/>
-					<input className="searchBar" placeholder={this.props.isLoading ? "Searching..."  : "Search recipes here..."}  type="text" value={this.state.search} style={this.props.isLoading ? {opacity: '0.4'} : {opacity: ''}}/>
-					<button className="searchBtn"><i className="fas fa-search"></i></button>
+					<input className="searchBar" placeholder={this.props.isLoading ? "Searching..."  : "Search recipes here..."}  type="text" value={this.state.search} onChange={this.searchUpdate} style={this.props.isLoading ? {opacity: '0.4'} : {opacity: ''}}/>
+					<button className="searchBtn" onClick={this.startSearch}><i className="fas fa-search" style={this.props.isLoading ? {animation: 'loading 1s infinite linear'} : {animation: ''}} ></i></button>
 				</form>
 			</div>
 		)
