fcff38804456633eb2f23e12f2d45fd8d8d1c428
esipode <chad@chaddyck.com>
2021-10-02 23:59:59 -0600

    Add updated build

---
+++ build/precache-manifest.4384dc06e645e8143b45f87acd9133da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1290e1c8f8b72e05ce85d1c85ba17fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f85c3a3c6f4489d62ee",
+    "url": "./static/css/main.2aa7df21.chunk.css"
+  },
+  {
+    "revision": "5e5fd9c454911024e34f",
+    "url": "./static/js/2.2536d232.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.2536d232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f85c3a3c6f4489d62ee",
+    "url": "./static/js/main.3eb9bf3b.chunk.js"
+  },
+  {
+    "revision": "09a3f965fa8c447a3f49",
+    "url": "./static/js/runtime-main.94ea6502.js"
+  }
+]);
